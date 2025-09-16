d3ba08f04a811a0585772ac6e777a9ad1b9ca941
PropYouLike <propyoulike@gmail.com>
2025-09-16 16:43:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   // 👇 Dev server settings
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
     strictPort: true,
     open: "/",
   },
